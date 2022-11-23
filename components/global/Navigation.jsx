7c7b256542c0380f7e1e2ea09e97adfa0b640d6e
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-11-23 21:05:45 +0100

    changes

--- components/global/Navigation.jsx
+++ components/global/Navigation.jsx
@@ -10,14 +10,12 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import AdbIcon from "@mui/icons-material/Adb";
-import SearchIcon from "@mui/icons-material/Search";
-import InputBase from "@mui/material/InputBase";
-import { useState } from "react";
-import { styled, alpha } from "@mui/material/styles";
+import { useState, useEffect } from "react";
 import { BsFillSunFill } from "react-icons/bs";
 import { FaMoon } from "react-icons/fa";
 import Link from "next/link";
 import { fetchQuery } from "./FetchQuery";
+import NavigationInput from "./NavigationInput";
 
 const pages = [
   {
@@ -27,9 +25,8 @@ const pages = [
 ];
 
 function ResponsiveAppBar({ lightMode, darkMode }) {
-  const { data, isLoading } = fetchQuery();
-  console.log(data?.items);
-  console.log(isLoading);
+  const { data } = fetchQuery();
+
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -39,48 +36,6 @@ function ResponsiveAppBar({ lightMode, darkMode }) {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "20ch",
-        },
-      },
-    },
-  }));
 
   return (
     <AppBar position="static" className="navbar">
@@ -143,15 +98,7 @@ function ResponsiveAppBar({ lightMode, darkMode }) {
             <FaMoon onClick={darkMode} className="moon" />
           </div>
           <Box sx={{ flexGrow: 0 }}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
+            <NavigationInput />
           </Box>
         </Toolbar>
       </Container>
